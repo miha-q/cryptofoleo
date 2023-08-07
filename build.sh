b1bf3ff25c0c8aaa1dd5fe5812e1647e2bfb884f
miha-q <>
2023-08-06 23:13:07 -0400

    Sun Aug  6 11:13:07 PM EDT 2023

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-gcc -shared -fPIC src/all.c -o bin/libFoleoCrypto.so -lgmp -DDEVICE='"/dev/random"'
-cp src/headers.h bin/foleo-crypto.h
+gcc -shared -fPIC src/all.c -o bin/libCryptoFoleo.so -lgmp -DDEVICE='"/dev/random"'
+cp src/headers.h bin/CryptoFoleo.h
